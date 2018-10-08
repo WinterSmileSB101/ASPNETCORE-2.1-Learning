2d24af88fb1febf8312afcde5e0a6ade0bbe5d54
SmileSB101 <zxb13194889618@outlook.com>
2018-10-08 23:50:08 +0800

    add [hello job] files

---
+++ Job/helloJob/helloJob/JobServices/HelloJobService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace helloJob.JobServices
+{
+    public class HelloJobService : IHostedService, IDisposable
+    {
+        private readonly ILogger _mLogger;
+        private Timer _mTimer;
+
+        public HelloJobService(ILogger<HelloJobService> logger) {
+            _mLogger = logger;
+        }
+
+        public void Dispose()
+        {
+            _mTimer?.Dispose();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _mLogger.LogInformation("work start");
+            _mTimer = new Timer(DoWork, null,TimeSpan.Zero, TimeSpan.FromSeconds(30));
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state) {
+            _mLogger.LogInformation("[{0:yyyy-MM-dd HH:mm:ss}] Timed background job is working now.", DateTime.Now);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _mLogger.LogInformation("background Task stoped.");
+            _mTimer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Job/helloJob/helloJob/Program.cs
@@ -0,0 +1,55 @@
+﻿using helloJob.JobServices;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using Microsoft.Extensions.Logging;
+
+namespace helloJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var host = BuildJobHost();
+            host.Run();
+        }
+
+        private static IHost BuildJobHost()
+        {
+            var host = new HostBuilder()
+                .ConfigureHostConfiguration(config =>
+                {
+                    // Microsoft.Extensions.Configuration
+                    config.SetBasePath(Directory.GetCurrentDirectory());
+
+                    config.AddEnvironmentVariables("ASPNETCORE_");
+                })
+                .ConfigureAppConfiguration((hostContext, configApp) =>
+                {
+                    configApp.AddJsonFile("appsettings.json", true);
+                    configApp.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", true);
+                    configApp.AddEnvironmentVariables();
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddLogging();
+                    services.AddHostedService<HelloJobService>();
+                })
+                .ConfigureLogging((hostContext, configLogging) =>
+                {
+                    // Microsoft.Extensions.Logging
+                    configLogging.AddConsole();
+                    if (hostContext.HostingEnvironment.EnvironmentName == EnvironmentName.Development) {
+                        // Microsoft.Extensions.Logging
+                        configLogging.AddDebug();
+                    }
+                })
+                .UseConsoleLifetime()
+                .Build();
+            return host;
+        }
+    }
+}
